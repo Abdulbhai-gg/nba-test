f2816576709036bbe05fbbaf888facaaa3beb9bb
Abdulbhai-gg <114620820+Abdulbhai-gg@users.noreply.github.com>
2022-12-20 17:22:25 +0500

    Add files via upload

---
+++ cr/copy.js
@@ -0,0 +1,239 @@
+function copy1(){
+   let copytext = document.querySelector('#copy1');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab1.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy2(){
+    let copytext = document.querySelector('#copy2');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy3(){
+    let copytext = document.querySelector('#copy3');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab3.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy4(){
+    let copytext = document.querySelector('#copy4');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab4.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy5(){
+    let copytext = document.querySelector('#copy5');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab5.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy6(){
+    let copytext = document.querySelector('#copy6');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab6.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy7(){
+    let copytext = document.querySelector('#copy7');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab7.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy8(){
+    let copytext = document.querySelector('#copy8');
+    navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab8.html");
+    copytext.style.backgroundColor='red'
+ }
+
+ function copy9(){
+   let copytext = document.querySelector('#copy9');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab9.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy10(){
+   let copytext = document.querySelector('#copy10');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab10.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy11(){
+   let copytext = document.querySelector('#copy11');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab11.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy12(){
+   let copytext = document.querySelector('#copy12');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab12.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy13(){
+   let copytext = document.querySelector('#copy13');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab13.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy14(){
+   let copytext = document.querySelector('#copy14');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab14.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy15(){
+   let copytext = document.querySelector('#copy15');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab15.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy16(){
+   let copytext = document.querySelector('#copy16');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab16.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy17(){
+   let copytext = document.querySelector('#copy17');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab17.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy18(){
+   let copytext = document.querySelector('#copy18');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab18.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy19(){
+   let copytext = document.querySelector('#copy19');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab19.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy20(){
+   let copytext = document.querySelector('#copy20');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab20.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy21(){
+   let copytext = document.querySelector('#copy21');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab21.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy22(){
+   let copytext = document.querySelector('#copy22');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab22.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy23(){
+   let copytext = document.querySelector('#copy23');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab23.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy24(){
+   let copytext = document.querySelector('#copy24');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab24.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy25(){
+   let copytext = document.querySelector('#copy25');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab25.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy26(){
+   let copytext = document.querySelector('#copy26');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab26.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy27(){
+   let copytext = document.querySelector('#copy27');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab27.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy28(){
+   let copytext = document.querySelector('#copy28');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab28.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy29(){
+   let copytext = document.querySelector('#copy29');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab29.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy30(){
+   let copytext = document.querySelector('#copy30');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab30.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy31(){
+   let copytext = document.querySelector('#copy31');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab31.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy32(){
+   let copytext = document.querySelector('#copy32');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab32.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy33(){
+   let copytext = document.querySelector('#copy33');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab33.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy34(){
+   let copytext = document.querySelector('#copy34');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab34.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy35(){
+   let copytext = document.querySelector('#copy35');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab35.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy36(){
+   let copytext = document.querySelector('#copy36');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab36.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy37(){
+   let copytext = document.querySelector('#copy37');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab37.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy38(){
+   let copytext = document.querySelector('#copy38');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab38.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy39(){
+   let copytext = document.querySelector('#copy39');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab39.html");
+   copytext.style.backgroundColor='red'
+}
+
+function copy40(){
+   let copytext = document.querySelector('#copy40');
+   navigator.clipboard.writeText("http://www.cr7streams.ml/p/ncaab40.html");
+   copytext.style.backgroundColor='red'
+}
