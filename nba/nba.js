5290a2b41fd53e6eb3b87465991df7ac848395a3
Abdulbhai-gg <114620820+Abdulbhai-gg@users.noreply.github.com>
2022-12-31 15:25:56 +0500

    Add files via upload

--- nba/nba.js
+++ nba/nba.js
@@ -3,56 +3,56 @@ nhl1.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width=
 
 
 let nba2 = document.querySelector('#nba2');
-nhl2.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba2.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba3 = document.querySelector('#nba3');
-nhl3.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba3.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba4 = document.querySelector('#nba4');
-nhl4.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba4.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba5 = document.querySelector('#nba5');
-nhl5.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba5.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba6 = document.querySelector('#nba6');
-nhl6.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba6.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba7 = document.querySelector('#nba7');
-nhl7.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba7.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba8 = document.querySelector('#nba8');
-nhl8.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba8.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba9 = document.querySelector('#nba9');
-nhl9.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba9.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba10 = document.querySelector('#nba10');
-nhl10.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba10.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba11 = document.querySelector('#nba11');
-nhl11.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba11.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba12 = document.querySelector('#nba12');
-nhl12.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba12.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba13 = document.querySelector('#nba13');
-nhl13.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba13.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba14 = document.querySelector('#nba14');
-nhl14.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba14.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
 
 
 let nba15 = document.querySelector('#nba15');
-nhl15.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+nba15.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
