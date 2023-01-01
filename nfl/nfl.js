9fd21e1212733faf04d3e63ff276c508a153a8b3
Abdulbhai-gg <114620820+Abdulbhai-gg@users.noreply.github.com>
2023-01-01 22:56:32 +0500

    Add files via upload

---
+++ nfl/nfl.js
@@ -0,0 +1,58 @@
+let nfl1 = document.querySelector('#nhl1');
+nfl1.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-6" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nfl2 = document.querySelector('#nhl2');
+nfl2.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-3" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nfl3 = document.querySelector('#nhl3');
+nfl3.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-7" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl4 = document.querySelector('#nhl4');
+nhl4.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-8" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl5 = document.querySelector('#nhl5');
+nhl5.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-2" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl6 = document.querySelector('#nhl6');
+nhl6.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-5" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl7 = document.querySelector('#nhl7');
+nhl7.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-9" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl8 = document.querySelector('#nhl8');
+nhl8.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-1" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl9 = document.querySelector('#nhl9');
+nhl9.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-4" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl10 = document.querySelector('#nhl10');
+nhl10.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-11" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl11 = document.querySelector('#nhl11');
+nhl11.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-10" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl12 = document.querySelector('#nhl12');
+nhl12.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-13" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`
+
+
+let nhl13 = document.querySelector('#nhl13');
+nhl13.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-12" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl14 = document.querySelector('#nhl14');
+nhl14.innerHTML = `<iframe src="http://cricfree.live/live/embed/nfl-14" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
+
+
+let nhl15 = document.querySelector('#nhl15');
+nhl15.innerHTML = `<iframe src="http://cricfree.live/live/embed/stream-40" width="100%" height="100%" allowfullscreen="true" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen></iframe>`;
